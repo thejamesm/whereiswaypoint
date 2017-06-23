1a09b4975f2e51470121efa1645005cdf4a61a3d
James Murray <thejamesm@users.noreply.github.com>
2017-06-22 20:50:49 -0400

    Bugfix: add two missing break statements

--- index.php
+++ index.php
@@ -30,10 +30,12 @@ switch (mt_rand(0, 8)) {
         $adverbs = file($adverbs_file, FILE_IGNORE_NEW_LINES);
         $adverb = $adverbs[mt_rand(0, count($adverbs) - 1)];
         $string = "The Waypoint URL is $adverb $link.";
+        break;
     case 3:
         $adverbs = file($adverbs_file, FILE_IGNORE_NEW_LINES);
         $adverb = $adverbs[mt_rand(0, count($adverbs) - 1)];
         $string = "The $adjective Waypoint URL is $adverb $link.";
+        break;
     default:
         if (mt_rand(0, 1)) $string = "The $adjective URL for Waypoint is $link";
         else $string = "The $adjective Waypoint URL is $link";
