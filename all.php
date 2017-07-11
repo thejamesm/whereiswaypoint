7fa6b878ffac8b71c8da19ac5b1cc9882550dde4
James Murray <thejamesm@users.noreply.github.com>
2017-07-11 17:25:49 -0400

    all.php: Get files from GitHub
    Was a silly oversight not to do it in the first place.

--- all.php
+++ all.php
@@ -2,12 +2,14 @@
 header('Content-Type:text/plain');
 
 $all = '';
+
+$p = 'https://raw.githubusercontent.com/thejamesm/whereiswaypoint/master';
 $files = array(
-    'urls.txt',
-    'live.txt',
-    'forums.txt',
-    'social.txt',
-    'music.txt'
+    "$p/urls.txt",
+    "$p/live.txt",
+    "$p/forums.txt",
+    "$p/social.txt",
+    "$p/music.txt"
 );
 
 foreach($files as $file) $all .= file_get_contents($file);
