482c82922fc5f373d8c1cfae020b9d502378dc92
James Murray <thejamesm@users.noreply.github.com>
2017-12-02 19:46:06 -0500

    Provide for expired URLs
    Most sections now added by a function (Twitter and Info are special
    cases done manually).

--- list/index.php
+++ list/index.php
@@ -1,16 +1,34 @@
 <?php
+
+function addSection($heading, $urls) {
+    echo "\t\t<section>\n";
+    echo "\t\t\t<h1>$heading</h1>\n";
+
+    foreach ($urls as $url) {
+        if ($url[0] == '!') {
+            $url = substr($url, 1);
+            echo "<span class=\"expired\">$url</span><br>\n";
+        } else {
+            echo "<a href=\"http://$url\">$url</a><br>\n";
+        }
+    }
+
+    echo "\t\t</section>\n\n";
+}
+
+
 $p = 'https://raw.githubusercontent.com/thejamesm/whereiswaypoint/master';
 
 $url_file = "$p/urls.txt";
 $urls = file($url_file, FILE_IGNORE_NEW_LINES);
 $live_file = "$p/live.txt";
-$lives = file($live_file, FILE_IGNORE_NEW_LINES);
+$live = file($live_file, FILE_IGNORE_NEW_LINES);
 $forum_file = "$p/forums.txt";
 $forums = file($forum_file, FILE_IGNORE_NEW_LINES);
 $social_file = "$p/social.txt";
-$socials = file($social_file, FILE_IGNORE_NEW_LINES);
+$social = file($social_file, FILE_IGNORE_NEW_LINES);
 $music_file = "$p/music.txt";
-$musics = file($music_file, FILE_IGNORE_NEW_LINES);
+$music = file($music_file, FILE_IGNORE_NEW_LINES);
 $twitter_acct_file = "$p/twitter_accts.txt";
 $twitter_accts = file($twitter_acct_file, FILE_IGNORE_NEW_LINES);
 
@@ -27,56 +45,18 @@ $discord = 'http://whereiswaypoint.info/groups';
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
-        <section>
-            <h1>Waypoint</h1>
 <?php
-foreach ($urls as $url) {
-    echo "<a href=\"http://$url\">$url</a><br>\n";
-}
+addSection('Waypoint', $urls);
+addSection('Live', $live);
+addSection('Forums', $forums);
+addSection('Social', $social);
+addSection('Music', $music);
 ?>
-        </section>
-
-        <section>
-            <h1>Live</h1>
-<?php
-foreach ($lives as $live) {
-    echo "<a href=\"http://$live\">$live</a><br>\n";
-}
-?>
-        </section>
-
-        <section>
-            <h1>Forums</h1>
-<?php
-foreach ($forums as $forum) {
-    echo "<a href=\"http://$forum\">$forum</a><br>\n";
-}
-?>
-        </section>
-
-        <section>
-            <h1>Social</h1>
-<?php
-foreach ($socials as $social) {
-        echo "<a href=\"http://$social\">$social</a><br>\n";
-}
-?>
-        </section>
-
-        <section>
-            <h1>Music</h1>
-<?php
-foreach ($musics as $music) {
-        echo "<a href=\"http://$music\">$music</a><br>\n";
-}
-?>
-        </section>
-
         <section>
             <h1>Twitter</h1>
 <?php
 foreach ($twitter_accts as $twitter_acct) {
-        echo "<a href=\"http://twitter.com/$twitter_acct\">@$twitter_acct</a><br>\n";
+    echo "<a href=\"http://twitter.com/$twitter_acct\">@$twitter_acct</a><br>\n";
 }
 ?>
         </section>
